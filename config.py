2a44d61e03d42697b003745ca3cdf6eb588810c3
Rashad <34587328+rashadibrahim@users.noreply.github.com>
2025-10-05 11:17:01 +0300

    Update SQLALCHEMY_DATABASE_URI format

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
     LOCAL_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}" #"sqlite:///fixed_assets.db"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwt-secret-key")
     # Build the database URI directly
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:yrbAGRBiCpuXYNQmPuUxmbiwQzruQrpt@postgres.railway.internal:5432/railway"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:yrbAGRBiCpuXYNQmPuUxmbiwQzruQrpt@postgres.railway.internal:5432/railway"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     # File upload configuration
